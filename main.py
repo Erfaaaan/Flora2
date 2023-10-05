3d96af574f0b3557c654f8077ab47eb58e2b8aef
Erfan Kiani <92972602+Erfaaaan@users.noreply.github.com>
2023-10-05 13:37:42 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ import base64
 import json
 import numpy as np
 import cv2
-
+from flora_ai import counting_flowers
 
 app = FastAPI()
 
-class AIModel:
-    @staticmethod
-    def process_image(image_b64: str,data_json: dict) -> dict:
-        # This is where your AI detection logic goes.
-        # For this example, we'll just return the input as output.
-        json_str = json.dumps(data_json)
-        #encoded_image = base64.b64encode(image_b64)
-        return {
-            "result_image": image_b64,
-            "result_data": json_str
-        }
+# class AIModel:
+#     @staticmethod
+#     def process_image(image_b64: str,data_json: dict) -> dict:
+#         # This is where your AI detection logic goes.
+#         # For this example, we'll just return the input as output.
+#         json_str = json.dumps(data_json)
+#         #encoded_image = base64.b64encode(image_b64)
+#         return {
+#             "result_image": image_b64,
+#             "result_data": json_str
+#         }
 
 @app.get("/")
 async def root():
@@ -34,8 +34,7 @@ async def upload():
 
 @app.post("/uploadfiles")
 async def upload_files(image_string: str = Body(...)):
-    # return "done"
-    # Decode the Base64 string
+    
     image_data = base64.b64decode(image_string)
     # Convert the image data to a numpy array
     nparr = np.frombuffer(image_data, np.uint8)
@@ -44,11 +43,12 @@ async def upload_files(image_string: str = Body(...)):
 
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+    json_str , image =  counting_flowers(image_rgb)
 
-    data_contents = {"test":"test"}
 
-    #image_b64 = base64.b64encode(image_contents).decode("utf-8")
+    
 
+   
     # convert the image to a memory buffer
     _, buffer = cv2.imencode('.jpg', image)
 
@@ -66,7 +66,7 @@ async def upload_files(image_string: str = Body(...)):
     }
 
 
-    #result = AIModel.process_image(image_encoded, data_contents)
+   
 
     return {"data":result,"message":"message","HTTPstatus":"httpstatus"}
 
