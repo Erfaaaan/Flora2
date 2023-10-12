5016da67c81eb861fd6ce6ab5b0d38d5db6dbb02
Erfan Kiani <92972602+Erfaaaan@users.noreply.github.com>
2023-10-12 22:25:24 +0330

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,7 @@ from flora_ai import counting_flowers
 
 app = FastAPI()
 
-# class AIModel:
-#     @staticmethod
-#     def process_image(image_b64: str,data_json: dict) -> dict:
-#         # This is where your AI detection logic goes.
-#         # For this example, we'll just return the input as output.
-#         json_str = json.dumps(data_json)
-#         #encoded_image = base64.b64encode(image_b64)
-#         return {
-#             "result_image": image_b64,
-#             "result_data": json_str
-#         }
+
 
 @app.get("/")
 async def root():
